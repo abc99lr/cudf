5aab9190c3e84b9f5aa78de2013857f957bb9213
Guillaume Thomas-Collignon <guillaumet@nvidia.com>
2020-12-16 10:48:27 -0800

    Add CUDA 11 support.

--- cpp/src/hash/concurrent_unordered_multimap.cuh
+++ cpp/src/hash/concurrent_unordered_multimap.cuh
@@ -93,7 +93,7 @@ public:
             cudaPointerAttributes hashtbl_values_ptr_attributes;
             cudaError_t status = cudaPointerGetAttributes( &hashtbl_values_ptr_attributes, m_hashtbl_values );
             
-            if ( cudaSuccess == status && hashtbl_values_ptr_attributes.isManaged ) {
+            if ( cudaSuccess == status && hashtbl_values_ptr_attributes.type == cudaMemoryTypeManaged ) {
                 int dev_id = 0;
                 CUDA_RT_CALL( cudaGetDevice( &dev_id ) );
                 CUDA_RT_CALL( cudaMemPrefetchAsync(m_hashtbl_values, m_hashtbl_size*sizeof(value_type), dev_id, 0) );
@@ -460,7 +460,7 @@ public:
         cudaPointerAttributes hashtbl_values_ptr_attributes;
         cudaError_t status = cudaPointerGetAttributes( &hashtbl_values_ptr_attributes, m_hashtbl_values );
         
-        if ( cudaSuccess == status && hashtbl_values_ptr_attributes.isManaged ) {
+        if ( cudaSuccess == status && hashtbl_values_ptr_attributes.type == cudaMemoryTypeManaged ) {
             CUDA_TRY( cudaMemPrefetchAsync(m_hashtbl_values, m_hashtbl_size*sizeof(value_type), dev_id, stream) );
         }
         CUDA_TRY( cudaMemPrefetchAsync(this, sizeof(*this), dev_id, stream) );
